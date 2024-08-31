f38c2ac578de085e8700b8aa3d4e7a0730dbefde
Mudit Saxena <mudit.25102010@gmail.com>
2024-08-31 20:15:57 +0530

    feat: added custom messages for validation errors

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/muditsaxena1/url-shortner/internal/models"
 	"github.com/muditsaxena1/url-shortner/internal/storage"
 )
@@ -17,7 +18,32 @@ func SetUser(c *gin.Context) {
 
 	// Bind the JSON body to the struct
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		errMessages := make(map[string]string)
+		if validationErrors, ok := err.(validator.ValidationErrors); ok {
+			for _, fieldErr := range validationErrors {
+				switch fieldErr.Field() {
+				case "ID":
+					if fieldErr.Tag() == "required" {
+						errMessages["id"] = "id is required"
+					}
+				case "Name":
+					if fieldErr.Tag() == "required" {
+						errMessages["name"] = "Name is required"
+					} else if fieldErr.Tag() == "min" {
+						errMessages["name"] = "name must be at least 2 characters long"
+					}
+				case "SignupTime":
+					if fieldErr.Tag() == "required" {
+						errMessages["signupTime"] = "Signup time is required"
+					} else if fieldErr.Tag() == "min" {
+						errMessages["signupTime"] = "signupTime must be after 1 Jan 1850"
+					}
+				}
+			}
+			c.JSON(http.StatusBadRequest, gin.H{"errors": errMessages})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
 		return
 	}
 
