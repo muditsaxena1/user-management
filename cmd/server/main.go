7f2610f72c87bed44ee20072f764fc4465248c85
Mudit Saxena <mudit.25102010@gmail.com>
2024-08-31 18:39:31 +0530

    feat: added port in cmd arg

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -8,10 +9,13 @@ import (
 )
 
 func main() {
+	port := flag.String("port", "8080", "server port")
+	flag.Parse()
+
 	router := gin.Default()
 
 	api.SetupRoutes(router)
 
-	log.Println("Server running on port 8080")
-	router.Run(":8080")
+	log.Printf("Server running on port %s", *port)
+	router.Run(":" + *port)
 }
